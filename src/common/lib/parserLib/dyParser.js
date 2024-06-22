947ecb44aeb57c86b0d5d00d8644952823fd33b1
wangzidong <wangzidong@meituan.com>
2024-06-22 12:00:37 +0800

    feat: 新增 a_bogus

--- src/common/lib/parserLib/dyParser.js
+++ src/common/lib/parserLib/dyParser.js
@@ -1,5 +1,6 @@
 
 import { request } from '../request';
+
 const MarkerUtilNativeModule = uni.requireNativePlugin('MarkerUtilUniPlugin-MarkerUtilNativeModule');
 
 
@@ -14,39 +15,55 @@ function generateRandomStr(randomLength = 107) {
     let randomStr = '';
     let baseStr = 'ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789=';
     let length = baseStr.length - 1;
-    for(let i = 0; i < randomLength; i++) {
+    for (let i = 0; i < randomLength; i++) {
         randomStr += baseStr[Math.floor(Math.random() * length)];
     }
     return randomStr;
 }
 
-function getMsToken(){
+function getMsToken() {
     return generateRandomStr(107)
 }
 
-export const sign = (url,user_agent) => {
+export const sign = (url, user_agent) => {
     console.log(url, user_agent);
-    return new Promise((res, rej)=>{
+    return new Promise((res, rej) => {
         MarkerUtilNativeModule.sign({
-            url, 
+            url,
             user_agent,
-        }, (ret)=>{
-            if(ret&&ret.data){
+        }, (ret) => {
+            if (ret && ret.data) {
                 res(ret.data);
-            }else{
+            } else {
                 rej('未知错误');
             }
         });
     });
 }
 
-export const getttwid = async (force = false)=>{
-    if (!ttwidCache || force){
+export const signFromHttp = async (url, ua) => {
+    console.log(url, ua);
+    const ret = await uni.request({
+        url: 'https://new.gagaprince.top/smallvideo/dyABogus',
+        method: 'POST',
+        data: {
+            url, ua
+        }
+    })
+    console.log('signFromHttp ret:', ret);
+    if (ret.statusCode === 200) {
+        return ret.data;
+    }
+    return ret;
+}
+
+export const getttwid = async (force = false) => {
+    if (!ttwidCache || force) {
         try {
             const ret = await request({
                 url: 'https://ttwid.bytedance.com/ttwid/union/register/',
                 method: 'POST',
-                data: {"migrate_info":{"ticket":"","source":"node"},"region":"cn","union":true,"service":"www.ixigua.com","aid":1768,"cbUrlProtocol":"https","needFid":false},
+                data: { "migrate_info": { "ticket": "", "source": "node" }, "region": "cn", "union": true, "service": "www.ixigua.com", "aid": 1768, "cbUrlProtocol": "https", "needFid": false },
                 headers: {
                     "Host": "ttwid.bytedance.com",
                     "accept": "application/json",
@@ -57,20 +74,21 @@ export const getttwid = async (force = false)=>{
             })
             const headers = ret.headers || {};
             const setCookie = headers['Set-Cookie'] || '';
+            console.log('getttwid headers:', headers);
             ttwidCache = parseTtwidFromCookie(setCookie);
-        }catch(e){
+        } catch (e) {
             console.error(e);
         }
     }
     return ttwidCache;
 }
 
-export const getRealLink = async (link)=>{
-    try{
+export const getRealLink = async (link) => {
+    try {
         const ret = await request({
-            url:link,
+            url: link,
             responseType: 'text',
-            followRedirects:0,
+            followRedirects: 0,
             headers: {
                 "Host": "v.douyin.com",
                 "pragma": "no-cache",
@@ -79,31 +97,31 @@ export const getRealLink = async (link)=>{
                 "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
                 "accept-language": "zh-CN,zh;q=0.9",
                 "cache-control": "no-cache"
-            },            
+            },
         })
         const headers = ret.headers || {};
         const location = headers['Location'] || '';
         return location;
-    }catch(e){
+    } catch (e) {
         console.error(e);
     }
     return '';
 }
 
-export const getAwemeId = async (originLink)=>{
+export const getAwemeId = async (originLink) => {
     const relink = await getRealLink(originLink);
-    if(relink){
+    if (relink) {
         let match = relink.match(/video\/(\d+)\//) || [];
         const awemeId = match[1] || '';
         return awemeId;
     }
     return '';
-} 
+}
 
 
 
-export const getVideoInfoByAwemeId = async (awemeId)=>{
-    const ttwid = await getttwid();
+export const getVideoInfoByAwemeId = async (awemeId) => {
+    const ttwid = await getttwid(true);
     const msToken = getMsToken();
     const headers = {
         "Host": "www.douyin.com",
@@ -113,36 +131,41 @@ export const getVideoInfoByAwemeId = async (awemeId)=>{
         "accept-language": "zh-Hans-CN;q=1, en-CN;q=0.9, zh-Hant-CN;q=0.8",
         "referer": "https://www.douyin.com/"
     };
-    const url = `https://www.douyin.com/aweme/v1/web/aweme/detail/?aweme_id=${awemeId}&aid=1128&version_name=23.5.0&device_platform=android&os_version=2333`;
+    const url = `https://www.douyin.com/aweme/v1/web/aweme/detail/?device_platform=webapp&aid=6383&channel=channel_pc_web&aweme_id=${awemeId}&update_version_code=170400&pc_client_type=1&version_code=190500&version_name=19.5.0&cookie_enabled=true&screen_width=1728&screen_height=1117&browser_language=zh-CN&browser_platform=MacIntel&browser_name=Chrome&browser_version=125.0.0.0&browser_online=true&engine_name=Blink&engine_version=125.0.0.0&os_name=Mac+OS&os_version=10.15.7&cpu_core_num=12&device_memory=8&platform=PC&downlink=10&effective_type=4g&round_trip_time=50`;
 
     console.log('url:', url);
 
-    try{
-        
-        const bogusObj = await sign(url, headers['user-agent']);
+    try {
 
-        console.log('bogus:', bogusObj);
-    
-        const newUrl = `${url}&X-Bogus=${bogusObj['X-Bogus']}`;
-    
-        console.log('newUrl:', newUrl);
-    
-        const ret = await request({
-            url: newUrl,
-            headers,
-        });
-    
-        console.log(ret);
-    
-        return ret && ret.data;
-    }catch (e){
+        const bogusObjRet = await signFromHttp(url, headers['user-agent']);
+        if (bogusObjRet.code === 0) {
+            const bogusObj = bogusObjRet.data;
+            console.log('bogus:', bogusObj);
+
+            const newUrl = bogusObj.url;
+
+            console.log('newUrl:', newUrl);
+            console.log('headers:', headers);
+
+            const ret = await request({
+                url: newUrl,
+                headers,
+            });
+
+            console.log(ret);
+
+            return ret && ret.data;
+        }
+        return {};
+
+    } catch (e) {
         console.error(e);
     }
-    
+
 
 }
 
-export const  getQueryString = (name, url) => {
+export const getQueryString = (name, url) => {
     let u = url || window.location.search;
     let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     if (u.substr(0, 1) === "?") {
@@ -155,7 +178,7 @@ export const  getQueryString = (name, url) => {
     return "";
 }
 
-export const getPercenVideos = async (secUserId, count, maxCursor=0)=>{
+export const getPercenVideos = async (secUserId, count, maxCursor = 0) => {
     const ttwid = await getttwid();
     const msToken = getMsToken();
     const headers = {
@@ -171,16 +194,16 @@ export const getPercenVideos = async (secUserId, count, maxCursor=0)=>{
 
     console.log('url:', url);
 
-    try{
-        
+    try {
+
         const bogusObj = await sign(url, headers['user-agent']);
 
         console.log('bogus:', bogusObj);
-    
+
         const newUrl = `${url}&X-Bogus=${bogusObj['X-Bogus']}`;
-    
+
         console.log('newUrl:', newUrl);
-    
+
         const ret = await request({
             url: newUrl,
             headers,
@@ -190,7 +213,7 @@ export const getPercenVideos = async (secUserId, count, maxCursor=0)=>{
         console.log(ret);
         // 太大的数据会打印不出来 所以不要打开
         return ret && ret.data;
-    }catch (e){
+    } catch (e) {
         console.error(e);
     }
 }
